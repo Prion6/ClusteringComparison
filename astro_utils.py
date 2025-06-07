a0e4036d5cace0468b324fe61dc60993e3a51176
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2025-06-07 16:39:25 -0400

    Update astro_utils.py
    method to get r_200

--- astro_utils.py
+++ astro_utils.py
@@ -1,6 +1,7 @@
 from astropy.cosmology import FlatLambdaCDM
 from astropy import units as u
 from astropy import constants as const
+import numpy as np
 
 def gal_Mpc_coords(ra, dec, redshift_gal, x_center, y_center, H0=70, Om0=0.3):
     
@@ -23,3 +24,20 @@ def los_vel(redshift_gal, redshift_clus):
     vel = (c*(redshift_gal - redshift_clus)/(1 + redshift_clus))
 
     return vel
+
+def get_r_200(log_m200, redshift, H0=70, Om0=0.3):
+    
+    cosmo = FlatLambdaCDM(H0=H0, Om0=Om0)
+
+    # Convert mass from log(Msun) to Msun
+    m200 = 10**log_m200 * u.Msun
+
+    # Get critical density at redshift z
+    rho_crit = cosmo.critical_density(redshift)  # in g/cm^3
+    rho_crit = rho_crit.to(u.Msun / u.Mpc**3)  # convert units
+
+    # Compute R200
+    r200 = ((3 * m200) / (4 * np.pi * 200 * rho_crit))**(1/3)
+    r200 = r200.to(u.Mpc).value
+
+    return r200
