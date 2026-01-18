f6858b7149d16f15bdf6a589849c1f70539201e4
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2026-01-18 13:33:40 -0300

    Codes Updated

--- astro_utils.py
+++ astro_utils.py
@@ -4,17 +4,17 @@ from astropy import constants as const
 import numpy as np
 
 def gal_Mpc_coords(ra, dec, redshift_gal, x_center, y_center, H0=70, Om0=0.3):
-    
     cosmo = FlatLambdaCDM(H0=H0, Om0=Om0)
-
     dist = cosmo.angular_diameter_distance(redshift_gal)
 
-    ra = ra - x_center
-    dec = dec - y_center
+    # Convert the differences to astropy Quantities with degree units
+    delta_ra = (ra - x_center) * u.deg
+    delta_dec = (dec - y_center) * u.deg
 
-    x = (ra * dist).to(u.Mpc, u.dimensionless_angles()).value
-
-    y = (dec * dist).to(u.Mpc, u.dimensionless_angles()).value
+    # Now the unit conversion to Mpc will use the correct 
+    # angular-to-physical conversion factor
+    x = (delta_ra * dist).to(u.Mpc, u.dimensionless_angles()).value
+    y = (delta_dec * dist).to(u.Mpc, u.dimensionless_angles()).value
 
     return x, y
 
